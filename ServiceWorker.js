4237b62aa52dff989d2b9c9aac272c3b35569ff1
Nikolai94 <laizr03@gmail.com>
2024-10-27 02:00:44 +0800

    V1.0.3 update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,51 +1,33 @@
-const cacheName = "Hyero-TarotSui-1.0.1"; // Update this version when needed
+const cacheName = "Hyero-TarotSui-1.0.3";
 const contentToCache = [
-    "Build/SUIV2_TG_V1.01.loader.js",
-    "Build/SUIV2_TG_V1.01.framework.js",
-    "Build/SUIV2_TG_V1.01.data",
-    "Build/SUIV2_TG_V1.01.wasm",
+    "Build/b61ce309569c307ed2344377e72b33ca.loader.js",
+    "Build/6be8e7eae18dd2fc6c7380e4a14b8e22.framework.js",
+    "Build/281870c33d56b03ac0945dcbc6199d09.data",
+    "Build/735b3bb420b356755a4407511a8e70fc.wasm",
     "TemplateData/style.css"
+
 ];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
-
+    
     e.waitUntil((async function () {
-        const cache = await caches.open(cacheName);
-        console.log('[Service Worker] Caching all: app shell and content');
-        await cache.addAll(contentToCache);
-    })());
-});
-
-self.addEventListener('activate', function (e) {
-    console.log('[Service Worker] Activate');
-
-    e.waitUntil((async function () {
-        // Get all cache keys
-        const cacheKeys = await caches.keys();
-
-        // Delete caches that are not the current version
-        await Promise.all(
-            cacheKeys.map(key => {
-                if (key !== cacheName) {
-                    console.log(`[Service Worker] Deleting old cache: ${key}`);
-                    return caches.delete(key);
-                }
-            })
-        );
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
     })());
 });
 
 self.addEventListener('fetch', function (e) {
     e.respondWith((async function () {
-        let response = await caches.match(e.request);
-        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-        if (response) { return response; }
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
 
-        response = await fetch(e.request);
-        const cache = await caches.open(cacheName);
-        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-        cache.put(e.request, response.clone());
-        return response;
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
     })());
 });
