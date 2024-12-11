f55e01501e787f8b48779f00d373f883485e8529
Nikolai94 <laizr03@gmail.com>
2024-12-12 00:47:28 +0800

    V1.1.1(Debug)

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,18 +1,33 @@
-<<<<<<< Updated upstream
-const cacheName = "Hyero-TarotSui-1.0.9(T)";
+const cacheName = "Hyero-TarotSui-1.1.1(Debug)";
 const contentToCache = [
-    "Build/08eb0946476ba52136b4d6df18c45c70.loader.js",
-    "Build/c990a72d04be7dd4ca5016e70618c725.framework.js",
-    "Build/a6a32cf71280eb891bb2fc80e95ba48f.data",
-    "Build/afe64912b10df96ab80f1512e0f8a5ce.wasm",
+    "Build/0ad906891ef2bbe777fa73f7ff89c9ed.loader.js",
+    "Build/2b172405ea66e259f36ceefe33f4fd57.framework.js",
+    "Build/07d9fab6bed97b6d9acdd836eff1d140.data",
+    "Build/3cddec005c088f4d746aa6d19be74e6f.wasm",
     "TemplateData/style.css"
 
 ];
-=======
->>>>>>> Stashed changes
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
