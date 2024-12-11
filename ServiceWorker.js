1096b93d20e7a3ba3386cf6ec00fc2cdd8fe14e7
Nikolai94 <laizr03@gmail.com>
2024-12-11 19:46:48 +0800

    Fix back to 1.0.9T

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "Hyero-TarotSui-1.0.9(T)";
 const contentToCache = [
     "Build/08eb0946476ba52136b4d6df18c45c70.loader.js",
     "Build/c990a72d04be7dd4ca5016e70618c725.framework.js",
-    "Build/458ba75e8f7a3270472307d3fee9eadd.data",
+    "Build/a6a32cf71280eb891bb2fc80e95ba48f.data",
     "Build/afe64912b10df96ab80f1512e0f8a5ce.wasm",
     "TemplateData/style.css"
 
