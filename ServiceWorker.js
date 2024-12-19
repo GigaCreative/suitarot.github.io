6f9f0a00b8a72915fc4dcc21e46776d608e01ea7
Nikolai94 <laizr03@gmail.com>
2024-12-19 18:27:48 +0800

    V1.1.9(Stage)

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "Hyero-TarotSui-1.1.8(Stage)";
+const cacheName = "Hyero-TarotSui-1.1.9(Stage)";
 const contentToCache = [
-    "Build/c1aae988a5beeaf68c2bb03a7e29cd86.loader.js",
-    "Build/dcb82f4c082e4bc4bad4599c2d75e945.framework.js",
-    "Build/779848e40291aab24e60a0902090b602.data",
-    "Build/2fbc37a63c08407c3bd1df1cb4fb0b85.wasm",
+    "Build/0e55e1baa3775f99371b8821ca316a16.loader.js",
+    "Build/0723ad5304e4cbc00007ed07f6ae3ae4.framework.js",
+    "Build/1bd8f2f4f533836d5c78956bd6ae1a9f.data",
+    "Build/86ef088acc0c2dd1fb9012217c1356da.wasm",
     "TemplateData/style.css"
 
 ];
